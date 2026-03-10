d1e9ee88c48a50b3a3d372f0a626d0809ee78e08
Daniel Ring <danielring.ch@googlemail.com>
2026-03-10 21:49:58 +0100

    Rework athentification handling again

--- src/modules/gui/gui.py
+++ src/modules/gui/gui.py
@@ -1,9 +1,11 @@
 import logging
+from fastapi import Request
+from functools import partial
 from nicegui import app, ui, Client
 from typing import Any
 
 from ..core import get_config_key, WEB_CONFIG_KEY, app_state
-from .login import auth_required, create_login_page, logout, HOME_PATH, LOGIN_PATH
+from .login import logins_by_session_id, auth_required, create_login_page, logout, HOME_PATH, LOGIN_PATH, get_session_and_instance_id
 from .models.homemodel import HomeModel
 from .models.schedulemodel import ScheduleModel
 from .models.settingsmodel import SettingsModel
@@ -34,7 +36,7 @@ class FakeModel:
     def destroy(self):
         pass
 
-models_by_client_id: dict[str, Any] = {}
+models_by_instance_id: dict[str, Any] = {}
 
 class Gui:
     def __init__(self, config: dict):
@@ -42,31 +44,31 @@ class Gui:
         self.__port = get_config_key(config, int, _PORT_ENV_NAME, WEB_CONFIG_KEY, _PORT_CONFIG_KEY)
 
         @ui.page(LOGIN_PATH)
-        def login_page():
-            client_id = ui.context.client.id
-            logging.debug(f'Client {client_id} created as login page.')
-            models_by_client_id[client_id] = FakeModel()
-            create_login_page()
+        def login_page(request: Request):
+            session_id, instance_id = get_session_and_instance_id(request)
+            logging.debug(f'session_id={session_id} instance_id={instance_id} created as login page.')
+            models_by_instance_id[instance_id] = FakeModel()
+            create_login_page(session_id)
         
         @ui.page(HOME_PATH)
         @auth_required
-        def home_page():
-            create_page(_HOME_NAME)
+        def home_page(request: Request):
+            create_page(_HOME_NAME, request)
 
         @ui.page(_SCHEDULE_PATH)
         @auth_required
-        def schedule_page():
-            create_page(_SCHEDULE_NAME)
+        def schedule_page(request: Request):
+            create_page(_SCHEDULE_NAME, request)
 
         @ui.page(_TEMPLATE_PATH)
         @auth_required
-        def template_page():
-            create_page(_TEMPLATE_NAME)
+        def template_page(request: Request):
+            create_page(_TEMPLATE_NAME, request)
 
         @ui.page(_SETTINGS_PATH)
         @auth_required
-        def settings_page():
-            create_page(_SETTINGS_NAME)
+        def settings_page(request: Request):
+            create_page(_SETTINGS_NAME, request)
 
     def run(self, storage_secret: str, startup_callback):
         app.on_startup(startup_callback)
@@ -82,18 +84,21 @@ class Gui:
             binding_refresh_interval=None,
             show=False)
         
-def create_page(tab_name: str):
-    client_id = ui.context.client.id
-    
-    if (old_model := models_by_client_id.get(client_id)):
-        logging.warning(f'Client {client_id} was reused.')
-        old_model.destroy()
-    logging.debug(f'Create page "{tab_name}" for client {client_id}')
-
+def create_page(tab_name: str, request: Request):
+    session_id, instance_id = get_session_and_instance_id(request)
+    user_name = logins_by_session_id.get(session_id)
+    if user_name is None:
+        logging.error(f'session_id={session_id} tricked the login check.')
+        ui.label('Access denied or invalid page.')
     try:
-        is_admin = app.storage.user.get('username') == app_state.data.admin_user.value
+        is_admin = user_name == app_state.data.admin_user.value
     except:
         is_admin = False
+
+    if (old_model := models_by_instance_id.get(instance_id)):
+        logging.warning(f'instance_id={instance_id} was reused.')
+        old_model.destroy()
+    logging.debug(f'Create page "{tab_name}" for session_id={session_id} instance_id={instance_id}')
 
     with ui.header().classes(replace='row items-center justify-center gap-2'):
         create_navigation_button(_HOME_NAME, HOME_PATH, tab_name)
@@ -101,34 +106,34 @@ def create_page(tab_name: str):
         create_navigation_button(_TEMPLATE_NAME, _TEMPLATE_PATH, tab_name)
         if is_admin:
             create_navigation_button(_SETTINGS_NAME, _SETTINGS_PATH, tab_name)
-        ui.button(_LOGOUT_NAME, on_click=logout).props('flat').classes('text-white')
+        ui.button(_LOGOUT_NAME, on_click=partial(logout, session_id)).props('flat').classes('text-white')
 
     # Route to the appropriate content based on the tab_name
     with ui.column().classes('w-full p-4'):
         if tab_name == _HOME_NAME:
-            model = HomeModel(client_id)
+            model = HomeModel(instance_id)
             create_home_tab(model)
         elif tab_name == _SCHEDULE_NAME:
-            model = ScheduleModel(client_id)
+            model = ScheduleModel(instance_id)
             create_schedule_tab(model)
         elif tab_name == _TEMPLATE_NAME:
-            model = TemplateModel(client_id)
+            model = TemplateModel(instance_id)
             create_template_tab(model)
         elif tab_name == _SETTINGS_NAME and is_admin:
-            model = SettingsModel(client_id)
+            model = SettingsModel(instance_id)
             create_settings_tab(model)
         else:
             model = FakeModel()
             ui.label('Access denied or invalid page.')
-    models_by_client_id[client_id] = model
+    models_by_instance_id[instance_id] = model
 
 def destroy_cliend(client: Client):
-    old_model = models_by_client_id.pop(client.id, None)
+    old_model = models_by_instance_id.pop(client.id, None)
     if not old_model:
-        logging.warning(f'Client {client.id} was double deleted.')
+        logging.warning(f'client_id={client.id} was double deleted.')
         return
     old_model.destroy()
-    logging.debug(f'Client {client.id} deleted.')
+    logging.debug(f'client_id={client.id} deleted.')
 
 def on_exception(e: Exception):
     logging.error(f'Exception from gui: {e}')
--- src/modules/gui/login.py
+++ src/modules/gui/login.py
@@ -1,7 +1,7 @@
 import inspect, logging
 from argon2.exceptions import VerifyMismatchError
+from fastapi import Request
 from fastapi.responses import RedirectResponse
-from functools import wraps
 
 from nicegui import app, ui
 
@@ -10,23 +10,27 @@ from ..core import app_state, password_hasher
 HOME_PATH = '/'
 LOGIN_PATH = '/login'
 
-def auth_required(func):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        if not app.storage.user.get('authenticated', False):
-            ui.navigate.to(LOGIN_PATH)
-            return
+logins_by_session_id: dict[str, str] = {}
 
-        if inspect.iscoroutinefunction(func):
-            return await func(*args, **kwargs)
+def auth_required(func):
+    async def wrapper(request: Request):
+        session_id, _ = get_session_and_instance_id(request)
+        if session_id not in logins_by_session_id:
+            ui.navigate.to(LOGIN_PATH)
         else:
-            return func(*args, **kwargs)
+            if inspect.iscoroutinefunction(func):
+                return await func(request)
+            else:
+                return func(request)
 
     return wrapper
 
-def create_login_page():
+def create_login_page(session_id: str):
     def try_login() -> None:  # local function to avoid passing username and password as arguments
         try:
+            if not session_id:
+                raise Exception('no session id')
+
             if username.value == app_state.data.admin_user.value:
                 hash = app_state.data.admin_pass.value
             elif username.value == app_state.data.user_user.value:
@@ -45,11 +49,12 @@ def create_login_page():
             ui.notify('Internal error.', color='negative')
             return
 
-        app.storage.user.update({'username': username.value, 'authenticated': True})
+        logins_by_session_id[session_id] = username.value
         ui.navigate.to(HOME_PATH)
 
-    if app.storage.user.get('authenticated', False):
+    if session_id in logins_by_session_id:
         return RedirectResponse(HOME_PATH)
+
     with ui.card().classes('absolute-center'):
         ui.label(app_state.data.instance_name.value)
         username = ui.input('Username').on('keydown.enter', try_login)
@@ -57,10 +62,19 @@ def create_login_page():
         ui.button('Log in', on_click=try_login)
     return None
 
-def logout():
-    app.storage.user.clear()
+def get_session_and_instance_id(request: Request):
+    session_id = str(request.session.get('id', ''))
+    if not session_id:
+        logging.error('Request did not contain an id.')
+    instance_id = ui.context.client.id
+    return session_id, instance_id
+
+def logout(session_id: str):
+    logging.debug(f'session_id={session_id} logged out')
+    logins_by_session_id.pop(session_id, None)
     ui.navigate.to(LOGIN_PATH)
 
 def logout_all():
-    app.storage.clear()
+    logging.debug('All users logged out.')
+    logins_by_session_id.clear()
     ui.navigate.to(LOGIN_PATH)
